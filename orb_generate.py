21cebe02d2b1a34945aa97776a4ee92adee79ca0
fishies <fishies@users.noreply.github.com>
2017-03-27 18:36:58 -0700

    Script now cleans up HTML scraped from Jagex site; output file size reduced by approx. 800 bytes. Effect on runtime is negligible

--- orb_generate.py
+++ orb_generate.py
@@ -35,7 +35,14 @@ def async_retrieveHTML(url):
     writer.write("GET {} HTTP/1.0\r\nHost: funorb.com\r\n\r\n".format(url).encode("UTF-8"))
     html = yield from reader.read()
     html = str(html, "ISO-8859-1")
-    return html[html.rfind("<tbody>"):html.find("</tbody>")]
+    return html[html.rfind("<tbody>")+len("<tbody>"):html.find("</tbody>")]
+
+def cleanEntry(html):
+    html = html.strip()
+    html = html.replace('a onclick="suffixize(this)"', "a")
+    html = html.replace(' class="row_b"', "")
+    html = html.replace(' class="row_c"', "")
+    return html
 
 #MAIN:
 start = time.time()
@@ -44,9 +51,9 @@ eventLoop = asyncio.get_event_loop()
 bodyHTML = eventLoop.run_until_complete(asyncio.gather(*[async_retrieveHTML(url) for url in GAMES]))
 eventLoop.close()
 
+bodyHTML = [cleanEntry(x) for x in bodyHTML]
+
 runtime = time.time() - start
 tableBody = "".join(bodyHTML)
 with open(OUTPUT_PAGE,mode="w") as file:
-    file.write("""<html><head><link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"><link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"><link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"><link rel="manifest" href="/manifest.json"><link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"><meta name="theme-color" content="#ffffff"><meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate, max-age=0"><meta http-equiv="Pragma" content="no-cache, no-store"><meta http-equiv="Expires" content="0"><title>FanOrb Central</title></head><body><table><thead><tr><th>Server</th><th>Location</th><th>Capacity</th><th>Players</th><th>Type</th></tr></thead>"""+tableBody+"""</tbody></table><p>This table was generated at """+time.asctime(time.gmtime(time.time()))+""" (GMT).<br>Script checks FunOrb servers approximately once per minute. <a href="http://binarydigit.io/orb">Refresh</a> to update.</p><p style="color:white;">"""+str(runtime)+"""</p></body></html>""")
-
-
+    file.write("""<html><head><link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"><link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"><link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"><link rel="manifest" href="/manifest.json"><link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"><meta name="theme-color" content="#ffffff"><meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate, max-age=0"><meta http-equiv="Pragma" content="no-cache, no-store"><meta http-equiv="Expires" content="0"><title>FanOrb Central</title></head><body><table><thead><tr><th>Server</th><th>Location</th><th>Capacity</th><th>Players</th><th>Type</th></tr></thead><tbody>"""+tableBody+"""</tbody></table><p>This table was generated at """+time.asctime(time.gmtime(time.time()))+""" (GMT).<br>Script checks FunOrb servers approximately once per minute. <a href="http://binarydigit.io/orb">Refresh</a> to update.</p><p style="color:white;">"""+str(runtime)+"""</p></body></html>""")
